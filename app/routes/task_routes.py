00eec03287373b984e0357c398a88531b98bc48b
Daniel De La Asuncion <danieldlasuncion@gmail.com>
2025-06-08 18:00:51 -0500

    Add import for Subject model in task_routes.py

--- app/routes/task_routes.py
+++ app/routes/task_routes.py
@@ -2,6 +2,7 @@ from flask import Blueprint, request, jsonify
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from app import db
 from app.models.task import Task
+from app.models.subject import Subject
 from app.models.task_schema import TaskSchema
 
 task_bp = Blueprint('task', __name__)
