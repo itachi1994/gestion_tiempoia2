3ec6cf171d78e02341e7116531f4150b8d293915
Daniel De La Asuncion <danieldlasuncion@gmail.com>
2025-06-13 19:51:07 -0500

    Add id field to SubjectSchema for serialization

--- app/models/subject_schema.py
+++ app/models/subject_schema.py
@@ -2,6 +2,7 @@ from app import ma
 from marshmallow import fields, validate
 
 class SubjectSchema(ma.Schema):
+    id         = fields.Integer(dump_only=True)
     name       = fields.String(required=True, validate=validate.Length(min=2))
     code       = fields.String(required=False)
     professor  = fields.String(required=False)
